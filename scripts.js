6a2ca66d79ab78ecb6c024886c0c6ad50244d201
Lesley White <lesley.white@outlook.com>
2023-07-31 17:34:42 +0100

    added button on click js styling

--- scripts.js
+++ scripts.js
@@ -1,7 +1,37 @@
-let button = document.getElementByTagName("button");
+//let button = document.getElementByTagName("button");
 
-turnButtonBlue = () => {
-    button.style.color = 'blue'
+//turnButtonBlue = () => {
+   // button.style.color = 'blue'
+//}
+
+//button.mouseover = turnButtonBlue;
+
+
+
+
+
+
+//THIS CODE ONLY WILL FIND THE FIRST BUTTON ELEMENT IN THE DOCUMENT
+
+
+//event handlers ( they do things when something is triggered)
+
+
+// DOM elements (to gain access to the DOM)
+let button = document.querySelector('button');
+
+//Define function (input is the event listener, output is the turning element a different colour) with event handler
+turnButtonGrey = () => {
+  button.style.backgroundColor = 'grey';
 }
 
-button.mouseover = turnButtonBlue;
+  turnButtonLilac = () => {
+    button.style.backgroundColor = 'rgb(219, 219, 230)';
+  }
+
+
+
+//Event listeners (they listen for certain things like button clicks)
+button.onmouseover = turnButtonGrey;
+button.onmouseout = turnButtonLilac;
+
